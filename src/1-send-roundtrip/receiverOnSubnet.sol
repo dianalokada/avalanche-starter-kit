a61acbe1cba95cc43dd26e6cf8125cf7503220ac
Diana Alokhina <62522159+dianalokada@users.noreply.github.com>
2024-06-29 22:41:27 +0000

    add IBLSSignatureVerifier to align with bls.sol from  evm precompiler

--- src/1-send-roundtrip/receiverOnSubnet.sol
+++ src/1-send-roundtrip/receiverOnSubnet.sol
@@ -4,8 +4,18 @@ pragma solidity ^0.8.18;
 import "@teleporter/ITeleporterMessenger.sol";
 import "@teleporter/ITeleporterReceiver.sol";
 
+interface IBLSSignatureVerifier {
+    function verifyBLSSignature(
+        string calldata message,
+        bytes calldata signature,
+        bytes calldata publicKey
+    ) external view returns (bool isValid);
+}
+
 contract ReceiverOnSubnet is ITeleporterReceiver {
     ITeleporterMessenger public immutable messenger = ITeleporterMessenger(0x253b2784c75e510dD0fF1da844684a1aC0aa5fcf);
+    
+    IBLSSignatureVerifier private constant BLS_VERIFIER = IBLSSignatureVerifier(0x0000000000000000000000000000000000000810); // Replace with the actual precompile address
 
     function receiveTeleporterMessage(bytes32 sourceBlockchainID, address originSenderAddress, bytes calldata message)
         external
@@ -13,18 +23,18 @@ contract ReceiverOnSubnet is ITeleporterReceiver {
         // Only the Teleporter receiver can deliver a message.
         require(msg.sender == address(messenger), "ReceiverOnSubnet: unauthorized TeleporterMessenger");
 
-        // Decode the incoming message
+        // Decoding the incoming message
         (string memory userMessage, bytes memory blsSignature, bytes memory blsPublicKey) =
             abi.decode(message, (string, bytes, bytes));
 
-        // Mock verification (always returns true)
-        bool verificationResult = mockVerifySignature(userMessage, blsSignature, blsPublicKey);
-
-        // Prepare the response message
+        // Verifying the signature
+        bool verificationResult = verifySignature(userMessage, blsSignature, blsPublicKey);
+        
+        // The response message
         string memory response =
             verificationResult ? "Signature verification successful" : "Signature verification failed";
 
-        // Send the verification result back to the C-chain
+        // Sends the verification result back to the C-chain
         messenger.sendCrossChainMessage(
             TeleporterMessageInput({
                 destinationBlockchainID: sourceBlockchainID,
@@ -37,12 +47,11 @@ contract ReceiverOnSubnet is ITeleporterReceiver {
         );
     }
 
-    function mockVerifySignature(string memory _message, bytes memory _signature, bytes memory _publicKey)
+    function verifySignature(string memory _message, bytes memory _signature, bytes memory _publicKey)
         internal
-        pure
+        view
         returns (bool)
     {
-        // Mock verification: always return true
-        return true;
+        return BLS_VERIFIER.verifyBLSSignature(_message, _signature, _publicKey);
     }
 }
